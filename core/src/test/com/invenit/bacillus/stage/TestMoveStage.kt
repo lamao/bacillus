1de22d55f6dbdcafb7b4666520380d7b43c6c145
invenit <vmischeryakov@gmail.com>
2021-11-30 16:50:09 +0200

    Draft for test

---
+++ core/src/test/com/invenit/bacillus/stage/TestMoveStage.kt
@@ -0,0 +1,18 @@
+package com.invenit.bacillus.stage
+
+import org.junit.jupiter.api.Test
+
+/**
+ *
+ * @author vyacheslav.mischeryakov
+ * Created: 30.11.21
+ */
+class TestMoveStage {
+
+    private val stage = MoveStage()
+
+    @Test
+    fun testRegularCase() {
+        // TODO: Implement
+    }
+}
