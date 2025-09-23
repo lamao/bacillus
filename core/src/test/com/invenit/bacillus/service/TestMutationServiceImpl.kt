3d3bca3c936d673d1e9d67f41af5fe914d6ed4fb
invenit <invenit.in.via@gmail.com>
2025-09-23 17:45:12 +0200

    Add tests

--- core/src/test/com/invenit/bacillus/service/TestMutationServiceImpl.kt
+++ core/src/test/com/invenit/bacillus/service/TestMutationServiceImpl.kt
@@ -1,16 +1,17 @@
 package com.invenit.bacillus.service
 
+import com.invenit.bacillus.model.DNA
 import com.invenit.bacillus.model.Substance
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.Mock
 import org.mockito.junit.jupiter.MockitoExtension
+import org.mockito.kotlin.whenever
 import kotlin.test.BeforeTest
 import kotlin.test.assertEquals
-import kotlin.test.assertNotEquals
 
 /**
- * Created by vyacheslav.mischeryakov
+ * Created by viacheslav.mishcheriakov
  * Created 08.12.2021
  */
 @ExtendWith(MockitoExtension::class)
@@ -27,8 +28,113 @@ class TestMutationServiceImpl {
     }
 
     @Test
-    fun testMutatedDna() {
-       TODO("Implement")
+    fun testNoMutation() {
+        val original = DNA(
+            body = Substance.Blue,
+            consume = Substance.Green,
+            produce = Substance.Yellow,
+            toxin = Substance.White,
+            canMove = false
+        )
+
+        whenever(mockRandomService.random()).thenReturn(1.0f)
+
+        val mutated = mutationService.mutatedDna(original)
+        assertEquals(original, mutated)
     }
 
+    @Test
+    fun testMutatedDnaBody() {
+        val original = DNA(
+            body = Substance.Blue,
+            consume = Substance.Sun,
+            produce = Substance.Green,
+            toxin = Substance.White,
+            canMove = true
+        )
+
+        whenever(mockRandomService.random()).thenReturn(0.0f)
+        whenever(mockRandomService.random(0, DNA.Trait.count() - 1)).thenReturn(0)
+        whenever(mockRandomService.random(1, Substance.values().size - 1))
+            .thenReturn(Substance.Red.ordinal)
+
+        val mutated = mutationService.mutatedDna(original)
+
+        assertEquals(original.copy(body = Substance.Red), mutated)
+    }
+
+    @Test
+    fun testMutatedDnaConsume() {
+        val original = DNA(
+            body = Substance.Blue,
+            consume = Substance.Blue,
+            produce = Substance.Green,
+            toxin = Substance.White,
+            canMove = false
+        )
+        whenever(mockRandomService.random()).thenReturn(0.0f)
+        whenever(mockRandomService.random(0, DNA.Trait.count() - 1)).thenReturn(1)
+        whenever(mockRandomService.random(0, Substance.values().size - 1))
+            .thenReturn(Substance.Sun.ordinal)
+
+        val mutated = mutationService.mutatedDna(original)
+
+        assertEquals(original.copy(consume = Substance.Sun), mutated)
+    }
+
+    @Test
+    fun testMutatedDnaProduce() {
+        val original = DNA(
+            body = Substance.Blue,
+            consume = Substance.Green,
+            produce = Substance.Blue,
+            toxin = Substance.White,
+            canMove = false
+        )
+        whenever(mockRandomService.random()).thenReturn(0.0f)
+        whenever(mockRandomService.random(0, DNA.Trait.count() - 1)).thenReturn(2)
+        whenever(mockRandomService.random(1, Substance.values().size - 1))
+            .thenReturn(Substance.Red.ordinal)
+
+        val mutated = mutationService.mutatedDna(original)
+
+        assertEquals(original.copy(produce = Substance.Red), mutated)
+    }
+
+    @Test
+    fun testMutatedDnaToxin() {
+        val original = DNA(
+            body = Substance.Blue,
+            consume = Substance.Green,
+            produce = Substance.Yellow,
+            toxin = Substance.Blue,
+            canMove = false
+        )
+        whenever(mockRandomService.random()).thenReturn(0.0f)
+        whenever(mockRandomService.random(0, DNA.Trait.count() - 1)).thenReturn(3)
+        whenever(mockRandomService.random(1, Substance.values().size - 1))
+            .thenReturn(Substance.White.ordinal)
+
+        val mutated = mutationService.mutatedDna(original)
+
+        assertEquals(original.copy(toxin = Substance.White), mutated)
+    }
+
+    @Test
+    fun testMutatedDna_CanMove() {
+        val original = DNA(
+            body = Substance.Blue,
+            consume = Substance.Green,
+            produce = Substance.Yellow,
+            toxin = Substance.White,
+            canMove = false
+        )
+        whenever(mockRandomService.random()).thenReturn(0.0f)
+        whenever(mockRandomService.random(0, DNA.Trait.count() - 1)).thenReturn(4)
+        whenever(mockRandomService.randomBoolean()).thenReturn(true)
+
+        val mutated = mutationService.mutatedDna(original)
+
+        assertEquals(original.copy(canMove = true), mutated)
+    }
 }
